d32decd3297841675a9e30b159224f6d4ab91bce
ACERTO\mauricio.souza <mauricio.souza@meuacerto.com.br>
2023-06-28 11:42:21 -0300

    Initialize option menu

--- SOLID/Program.cs
+++ SOLID/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Choose a SOLID principle");
+Console.WriteLine("1 - S (Single Responsibility)");
+Console.WriteLine("2 - O (Open-Closed)");
+Console.WriteLine("3 - L (Liskov Substitution)");
+Console.WriteLine("4 - I (Interface Segregation)");
+Console.WriteLine("5 - D (Dependency Inversion)");
+
+var opcao = Console.ReadKey();
+
+switch (opcao.KeyChar)
+{
+    case '1':
+        //CaixaEletronico.Operacoes();
+        break;
+    case '2':
+        //CalculoArea.Calcular();
+        break;
+    case '3':
+        //CalculoArea.Calcular();
+        break;
+    case '4':
+        //CalculoArea.Calcular();
+        break;
+    case '5':
+        //CalculoArea.Calcular();
+        break;
+}
